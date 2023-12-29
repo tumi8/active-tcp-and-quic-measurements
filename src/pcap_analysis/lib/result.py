9faf8f493c94d45859e167635537aeab08e862a1
bauersi <34128019+bauersi@users.noreply.github.com>
2023-12-29 09:56:36 +0100

    removed rca (requires further updates)

--- src/pcap_analysis/lib/result.py
+++ src/pcap_analysis/lib/result.py
@@ -5,7 +5,6 @@ from lib.target import TargetInfo
 from lib.tcp_options import TCPOptions
 from lib.tcp_flow_info import TCPFlowCharacteristics
 from lib.tcp_kpis import TCPKPIs
-from lib.tcp_rca import TCPRCA
 from lib.quic_kpis import QUICRESULTs
 
 class Result: 
@@ -22,7 +21,6 @@ class Result:
 
                 self.tcp_path = "{}/{}/{}".format(basepath + "/tcp_downloads", domain, run_id)
                 self.quic_path = "{}/{}".format(basepath + "/quic_downloads", domain)
-   
 
                 #try: 
                 self.capture = Capture(self.tcp_path, self.debug) 
@@ -33,13 +31,11 @@ class Result:
                     self.flow_chars = None
                     self.tcp_kpis = None
                     self.quic_kpis = None
-                    self.tcp_rca = None
                 self.target = TargetInfo(self.tcp_path, self.debug) 
                 self.tcpoptions = TCPOptions(self.capture.capture_df, self.debug)
                 self.tcpflowchars = TCPFlowCharacteristics(self.capture.capture_df, self.capture.ips)
                 self.tcpkpis = TCPKPIs(basepath, domain, run_id, self.capture.capture_df, self.capture.ips, self.debug)
                 self.quicresults = QUICRESULTs(self.quic_path)
-                self.tcprca = TCPRCA(basepath, domain, run_id, self.capture.capture_df, self.capture.ips, self.debug)
 
             except Exception as e: 
                 print("EXCEPTION - reading pcap and creating capture instance failed")
@@ -50,7 +46,6 @@ class Result:
                 self.tcpflowchars = None
                 self.tcpkpis = None
                 self.quicresults = None
-                self.tcprca = None
             
         else: 
             self.run_id = ""
@@ -64,7 +59,6 @@ class Result:
             self.tcpoptions = TCPOptions(self.capture.capture_df, self.debug)
             self.tcpflowchars = TCPFlowCharacteristics(self.capture.capture_df, self.capture.ips)
             self.tcpkpis = TCPKPIs(basepath, domain, run_id, self.capture.capture_df, self.capture.ips, self.debug)
-            self.tcprca = TCPRCA(basepath, domain, run_id, self.capture.capture_df, self.capture.ips, self.debug)
 
     def get_output_dict(self):   
 
@@ -156,34 +150,6 @@ class Result:
                     "tcp_retransmission_rate": self.tcpkpis.retransmission_stats.total_retransmission_rate,
                     "tcp_dtt": self.tcpkpis.dtt,
                     "tcp_cst": self.tcpkpis.cst,
-                #"rca": { 
-                    "tcp_rca_number_alp":       self.tcprca.number_alp,     
-                    "tcp_rca_number_ub":        self.tcprca.number_ub,      
-                    "tcp_rca_number_sb":        self.tcprca.number_sb,      
-                    "tcp_rca_number_rw":        self.tcprca.number_rw,      
-                    "tcp_rca_number_tl":        self.tcprca.number_tl,      
-                    "tcp_rca_number_other":     self.tcprca.number_other,   
-                    "tcp_rca_bytes_alp":        self.tcprca.bytes_alp,      
-                    "tcp_rca_bytes_ub":         self.tcprca.bytes_ub,       
-                    "tcp_rca_bytes_sb":         self.tcprca.bytes_sb,       
-                    "tcp_rca_bytes_rw":         self.tcprca.bytes_rw,       
-                    "tcp_rca_bytes_tl":         self.tcprca.bytes_tl,       
-                    "tcp_rca_bytes_other":      self.tcprca.bytes_other,   
-                    "tcp_rca_duration_alp":     self.tcprca.duration_alp,   
-                    "tcp_rca_duration_ub":      self.tcprca.duration_ub,    
-                    "tcp_rca_duration_sb":      self.tcprca.duration_sb,    
-                    "tcp_rca_duration_rw":      self.tcprca.duration_rw,    
-                    "tcp_rca_duration_tl":      self.tcprca.duration_tl,    
-                    "tcp_rca_duration_other":   self.tcprca.duration_other, 
-                    "tcp_rca_packets_alp":      self.tcprca.packets_alp,    
-                    "tcp_rca_packets_ub":       self.tcprca.packets_ub,     
-                    "tcp_rca_packets_sb":       self.tcprca.packets_sb,     
-                    "tcp_rca_packets_rw":       self.tcprca.packets_rw,     
-                    "tcp_rca_packets_tl":       self.tcprca.packets_tl,    
-                    "tcp_rca_packets_other":    self.tcprca.packets_other,
-                    "tcp_rca_max_outstanding":  self.tcprca.max_outstanding,  
-                    "tcp_rca_min_awnd":         self.tcprca.min_awnd,
-                    "tcp_rca_max_awnd":         self.tcprca.max_awnd,
             }
   
         return output
